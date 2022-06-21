940a23118ff6121557d8d878f0f009cab058b5a8
Bwnr-Pwnr <zaxdev59@gmail.com>
2022-06-21 16:27:14 -0700

    Finished most of it, user functions and testimonials next

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   // return my header, footer and stuff here to persist
   return (<>
-    
+
     <Header />
     <Component {...pageProps} />
     <Footer />
